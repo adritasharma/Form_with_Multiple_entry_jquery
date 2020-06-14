f70969ebe38470ab94269972a1b361c445d75af6
Adrita Sharma <adritasharma.developer@gmail.com>
2020-06-14 18:27:45 +0530

    gettting form data dynamically

---
+++ js/task.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    // Getting Navbar Html Dynamically from utility.js
+    var navData = createNavbar();
+
+    $(".navbar").append(navData);
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+        var values = formatFormData("form",true,"skill-item","skills");
+        console.log(values)
+
+    });
+
+    $('#addSkillBtn').click(function () {
+        $('.skill-item:last').clone().find("input").val("").end().appendTo('#skillsContainer')
+    });
+
+
+    $(document).on('click', ".remove-skill", function () {
+        var skillCount = $('.skill-item').length;
+        console.log(skillCount);
+        if (skillCount > 1) {
+            (this).closest(".skill-item").remove();
+        }
+        if (skillCount == 2) {
+            $('.remove-skill').attr('disabled', true);
+        }
+    });
+
+});
+
+
+
