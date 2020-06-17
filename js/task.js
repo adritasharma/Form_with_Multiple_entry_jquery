cd35b208dbc9eda228c0bad8c84334f280136ffb
Adrita Sharma <adritasharma.developer@gmail.com>
2020-06-17 22:28:17 +0530

    remove functionality made generic

--- js/task.js
+++ js/task.js
@@ -1,31 +1,21 @@
 $(document).ready(function () {
 
-    // Getting Navbar Html Dynamically from utility.js
-    var navData = createNavbar();
+    $('#addSkillBtn').click(function () {
+        cloneArrayItem('skillsContainer', 'skill-item');
+        $('.remove-skill').attr('disabled', false);
+    });
 
-    $(".navbar").append(navData);
+
+    $(document).on('click', ".remove-skill", function () {
+        handleFormArrayDelete(this, "skill-item", "remove-skill");
+    });
 
     $("form").submit(function (e) {
         e.preventDefault();
         var values = formatFormData("form", true, "skill-item", "skills");
         console.log(values)
-
-    });
-
-    $('#addSkillBtn').click(function () {
-        cloneArrayItem('skillsContainer', 'skill-item');
-    });
-
-
-    $(document).on('click', ".remove-skill", function () {
-        var skillCount = $('.skill-item').length;
-        console.log(skillCount);
-        if (skillCount > 1) {
-            (this).closest(".skill-item").remove();
-        }
-        if (skillCount == 2) {
-            $('.remove-skill').attr('disabled', true);
-        }
+        $('#result').empty();
+        $('#result').append(JSON.stringify(values));
     });
 
 });
--- js/utility.js
+++ js/utility.js
@@ -1,113 +1,11 @@
-/**
-   * Handles show/hide functionality of each item.
-   *
-   * @param {string} changeIcon
-   *   true/false to indicate if any icon should be changed.
-   * @param {string} iconClass1
-   *   the first icon to be toggled.
-   * @param {string} iconClass2
-   *   the second icon to be toggled.
-   *
-*/
-function handleAccordion(changeIcon, iconClass1, iconClass2) {
-    var acc = document.getElementsByClassName("accordion");
-    var i;
 
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            if (changeIcon) {
-                $("i.panel-icon", this).toggleClass(`${iconClass1} ${iconClass2}`);
-            }
-            $(".panel", this).toggle();
-        });
-
-    }
-}
-
-function cloneArrayItem(containerIdName, classToClone){
+function cloneArrayItem(containerIdName, classToClone) {
     $(`.${classToClone}:last`).clone().find("input").val("").end().appendTo(`#${containerIdName}`)
 }
 
-/**
-   * Creates Navbar HTML based on json Data
-   *
-*/
-function createNavbar() {
-
-    var navData = [
-        {
-            text: "Home",
-            link: "#home",
-            icon: "fa fa-home",
-            showDropDown: false
-        },
-        {
-            text: "Invoices & Payments",
-            icon: "fa fa-file-o",
-            showDropDown: true,
-            dropdowns: [
-                {
-                    text: "Invoice",
-                    link: "#",
-                    icon: ""
-                },
-                {
-                    text: "Payment",
-                    link: "#",
-                    icon: ""
-                }
-            ]
-        },
-        {
-            text: "Contracts",
-            icon: "fa fa-bookmark-o",
-            showDropDown: true,
-            dropdowns: [
-                {
-                    text: "View Contracts",
-                    link: "#",
-                    icon: ""
-                },
-                {
-                    text: "Manage Documents",
-                    link: "#",
-                    icon: ""
-                }
-            ]
-        },
-        {
-            text: "Applications",
-            link: "#application",
-            icon: "fa fa-file-text-o",
-            showDropDown: false
-        }
-    ]
-    var navContent = ``;
-
-    $.each(navData, function (index, nav) {
-        if (!nav.showDropDown) {
-            navContent += `<a href="${nav.link}"><i class="${nav.icon}"></i> ${nav.text}</a>`
-        } else {
-            navContent += ` 
-            <div class="dropdown">
-                <button class="dropbtn"><i class="${nav.icon}"></i> ${nav.text}
-                    <i class="fa fa-angle-down"></i>
-                </button>
-                <div class="dropdown-content">`
-
-            $.each(nav.dropdowns, function (index, dropdown) {
-                navContent += `<a href="${dropdown.link}"><i class="${dropdown.icon}"></i> ${dropdown.text}</a>`
-            })
-            navContent += `</div>
-            </div>`
-        }
-    });
-    return navContent;
-}
 
 
-function formatFormData(formSelector, containsFormArray,repeatContainerClass, formArrayPropertyName) {
+function formatFormData(formSelector, containsFormArray, repeatContainerClass, formArrayPropertyName) {
     var values = {};
     $.each($(formSelector).serializeArray(), function (i, field) {
         if (containsFormArray) {
@@ -122,6 +20,7 @@ function formatFormData(formSelector, containsFormArray,repeatContainerClass, fo
     values[formArrayPropertyName] = formatFormArrayData(repeatContainerClass);
     return values;
 }
+
 
 function formatFormArrayData(repeatContainerClass) {
     let formArray = [];
@@ -141,4 +40,14 @@ function formatFormArrayData(repeatContainerClass) {
 
 }
 
+function handleFormArrayDelete(that,repeatClassName, deleteButtonClassName) {
+    var itemCount = $(`.${repeatClassName}`).length;
+    console.log(itemCount);
+    if (itemCount > 1) {
+        (that).closest(`.${repeatClassName}`).remove();
+    }
+    if (itemCount == 2) {
+        $(`.${deleteButtonClassName}`).attr('disabled', true);
+    }
+}
 
